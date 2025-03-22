68d948989aece3a0ea9b3aaf7c5593f42648322f
Anas <aanu8148@gmail.com>
2025-03-22 16:59:03 +0530

    product of array except self challenge day 6

---
+++ Challenge-5/product-of-arry-except-self.py
@@ -0,0 +1,17 @@
+def productExceptSelf(nums):
+        
+    res=[1]*(len(nums))
+    prefix=1
+    for i in range(len(nums)):
+        res[i]=prefix
+        prefix *=nums[i]
+    postfix=1
+    for j in range(len(nums)-1,-1,-1):
+        res[j] *=postfix
+        postfix *=nums[j]
+
+    return res
+
+
+nums=[1,2,3,4]
+print(productExceptSelf(nums))            
